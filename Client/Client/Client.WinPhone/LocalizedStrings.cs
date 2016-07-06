f382a8d6e755ef1d124a893092ec01b911ccace7
Veronica Gliga <veronica.gliga@tecknoworks.com>
2016-07-06 11:58:40 +0300

    Initial solution

---
+++ Client/Client/Client.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Client.WinPhone.Resources;
+
+namespace Client.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
