3b6d21812ce2cd8185beb40009afb6352a041e95
Fertea Tudor Andrei <tudorandrei.fertea@tecknoworks.com>
2016-07-11 14:50:02 +0300

    Repository interface and repository for client

---
+++ Server/Repository/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    interface IRepository <TEntity>
+    {
+        TEntity get(int id);
+        IEnumerable<TEntity> GetaAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity,bool>> predicate);
+
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+        
+
+    }
+}
