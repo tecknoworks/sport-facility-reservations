72e8f99c01adc362b215fbad88e87434a5e8fa28
Veronica Gliga <veronica.gliga@tecknoworks.com>
2016-07-11 15:08:51 +0300

    Login method created in IServiceClient interface and ServiceClient class

---
+++ Client/Client/Client/Services/ServiceClient.cs
@@ -0,0 +1,17 @@
+﻿using Client.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Services
+{
+    class ServiceClient: IServiceClient
+    {
+        public string Login(string username, string password)
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
