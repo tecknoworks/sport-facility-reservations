b358ee9888e99011f50e9f8112fd8b23a6dabc2b
Fertea Tudor Andrei <tudorandrei.fertea@tecknoworks.com>
2016-07-18 16:50:03 +0300

    Edit Global.asax.cs to allow acces without migrations.

--- Server/Server/Global.asax.cs
+++ Server/Server/Global.asax.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using Repository.Models;
+using System.Data.Entity;
 
 namespace Server
 {
@@ -12,6 +14,7 @@ namespace Server
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            Database.SetInitializer(new FacilityInitializer() );
         }
     }
 }
