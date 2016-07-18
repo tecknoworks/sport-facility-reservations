95eb3b9e0d2d070a93a7708c5712a097582cb9f8
Veronica Gliga <veronica.gliga@tecknoworks.com>
2016-07-18 10:59:40 +0300

    Added Prism.Forms

--- Client/Client/Client.Droid/Resources/Resource.Designer.cs
+++ Client/Client/Client.Droid/Resources/Resource.Designer.cs
@@ -26,6 +26,8 @@ namespace Client.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.Forms.Platform.Resource.String.ApplicationName = global::Client.Droid.Resource.String.ApplicationName;
+			global::Xamarin.Forms.Platform.Resource.String.Hello = global::Client.Droid.Resource.String.Hello;
 		}
 		
 		public partial class Attribute
@@ -81,11 +83,30 @@ namespace Client.Droid
 			}
 		}
 		
+		public partial class String
+		{
+			
+			// aapt resource value: 0x7f030001
+			public const int ApplicationName = 2130903041;
+			
+			// aapt resource value: 0x7f030000
+			public const int Hello = 2130903040;
+			
+			static String()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private String()
+			{
+			}
+		}
+		
 		public partial class Style
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int Mono_Android_Theme_Splash = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int Mono_Android_Theme_Splash = 2130968576;
 			
 			static Style()
 			{
