278637602df1c8a66775e5afd55345ea1ff0624b
Veronica Gliga <veronica.gliga@tecknoworks.com>
2016-07-18 14:47:46 +0300

    The client solution/project was broken, so I fixed it.

--- Client/Client/Client/Views/LoginPage.cs
+++ Client/Client/Client/Views/LoginPage.cs
@@ -10,7 +10,7 @@ using Microsoft.Practices.Unity;
 
 namespace Client.Views
 {
-    public class LoginPage
+    public class LoginPage : ContentPage
     {
         public Image Img { get; set; }
         public LoginViewModel _viewModel;
@@ -39,7 +39,6 @@ namespace Client.Views
                     new RowDefinition { Height = GridLength.Auto },
                     new RowDefinition { Height = GridLength.Auto },
                     new RowDefinition { Height = GridLength.Auto },
-                    new RowDefinition { Height = GridLength.Auto },
                     new RowDefinition { Height = GridLength.Auto }
                 },
                 ColumnDefinitions =
@@ -54,7 +53,7 @@ namespace Client.Views
             var usernameLabel = new Label
             {
                 Text = "Username",
-                HorizontalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.Center,
                 VerticalOptions = LayoutOptions.CenterAndExpand
             };
             grid.Children.Add(usernameLabel, 0, 2);
@@ -62,64 +61,69 @@ namespace Client.Views
             var passwordLabel = new Label
             {
                 Text = "Password",
-                HorizontalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.Center,
                 VerticalOptions = LayoutOptions.CenterAndExpand
             };
-            grid.Children.Add(passwordLabel, 0, 4);
+            grid.Children.Add(passwordLabel, 0, 3);
 
             _usernameEntry = new Entry
             {
                 Placeholder = "Fill in your username",
-                Keyboard = Keyboard.Default              
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                Keyboard = Keyboard.Default
             };
-            grid.Children.Add(_usernameEntry, 0, 3);
+            grid.Children.Add(_usernameEntry, 1, 2);
             _usernameEntry.SetBinding(Entry.TextProperty, "Username");
 
             var passwordEntry = new Entry
             {
                 Placeholder = "Fill in your password",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
                 Keyboard = Keyboard.Default,
                 IsPassword = true
             };
-            grid.Children.Add(passwordEntry, 0, 5);
+            grid.Children.Add(passwordEntry, 1, 3);
             passwordEntry.SetBinding(Entry.TextProperty, "Password");
 
-            var button = new Button
+            var loginButton = new Button
             {
                 Text = "Login",
                 FontSize = 10,
                 BorderWidth = 1,
                 WidthRequest = 100,
                 HeightRequest = 30,
-                HorizontalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.Center,
                 VerticalOptions = LayoutOptions.CenterAndExpand
             };
-            grid.Children.Add(button, 0, 6);
-            button.Clicked += Button_Clicked;
+            grid.Children.Add(loginButton, 1, 4);
+            loginButton.Clicked += Button_Clicked;
 
             var registerLabel = new Label
             {
                 Text = "Don't have an account?",
                 FontSize = 10,
-                HorizontalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.Center,
                 VerticalOptions = LayoutOptions.CenterAndExpand
             };
-            grid.Children.Add(registerLabel, 0, 7);
+            grid.Children.Add(registerLabel, 1, 6);
 
 
-            var button2 = new Button
+            var registerButton = new Button
             {
                 Text = "Register",
                 FontSize = 10,
                 WidthRequest = 100,
                 HeightRequest = 30,
                 BorderWidth = 1,
-                HorizontalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.Center,
                 VerticalOptions = LayoutOptions.CenterAndExpand
 
             };
-            grid.Children.Add(button2, 0, 8);
-            button2.Clicked += Button_Clicked2;
+            grid.Children.Add(registerButton, 1, 7);
+            registerButton.Clicked += Button_Clicked2;
+            
             this.Content = grid;
         }
         private async void Button_Clicked(object sender, EventArgs e)
