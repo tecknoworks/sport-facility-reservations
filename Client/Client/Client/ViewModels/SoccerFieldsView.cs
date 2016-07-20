56b9946f900610bfb9160dc9fc6abf768e8084a3
Veronica Gliga <veronica.gliga@tecknoworks.com>
2016-07-20 10:16:25 +0300

    Modified SoccerFieldsView class

--- Client/Client/Client/ViewModels/SoccerFieldsView.cs
+++ Client/Client/Client/ViewModels/SoccerFieldsView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Android.Widget;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,6 +18,8 @@ namespace Client.ViewModels
 
         public async Task Init()
         {
+           
+
             var label = new Label
             {
                 Text = "Hello"
