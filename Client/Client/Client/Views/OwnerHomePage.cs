e33696a35f0ceb5169d0df09dbfeb5f5457c94db
Veronica Gliga <veronica.gliga@tecknoworks.com>
2016-07-25 16:08:32 +0300

    OwnerHomePage - that represents a tabbed page from which the owner will see the reservations made for the fields he owns, or he can navigate to the regular HomePage; SerachPage - deleted the background color

---
+++ Client/Client/Client/Views/OwnerHomePage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Microsoft.Practices.Unity;
+using Client.ViewModels;
+
+namespace Client.Views
+{
+    class OwnerHomePage : TabbedPage
+    {
+        public OwnerHomePage()
+        {
+            Children.Add(new ReservationPage());
+            Children.Add(new HomePage("Hi"));
+        }
+
+    }
+}
