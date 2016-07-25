35079ea910baf4c625b13a2e4c09a22530f1bee2
Veronica Gliga <veronica.gliga@tecknoworks.com>
2016-07-25 11:30:54 +0300

    LoginController - login method created

--- Server/Server/Controllers/LoginController.cs
+++ Server/Server/Controllers/LoginController.cs
@@ -14,6 +14,7 @@ using Repository;
 
 namespace Server.Controllers
 {
+    [RoutePrefix("api/Login")]
     public class LoginController : ApiController
     {
         // public FacilityContext _context = new FacilityContext();
@@ -23,8 +24,74 @@ namespace Server.Controllers
         {
             return unitOfWork.userRepository.GetAll();
         }
+        
+        [Route("GetUserById")]
+        public IHttpActionResult GetUserById(int id)
+        {
+            var user = GetUsers().FirstOrDefault((p) => p.ID == id);
+            if(user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
 
+        [Route("GetUserByName")]
+        public IHttpActionResult GetUserByName(string name, string password)
+        {
+            var userr = GetUsers().FirstOrDefault((q) => q.Name.Equals(name)&& q.Password.Equals(password));
+            if(userr == null)
+            {
+                return NotFound();
+            }
+            return Ok(userr);
+        }
+        
+       [HttpPut]
+        public void Put(string name/*, string password*/)
+        {
 
+            User user1 = new User();
+            user1.Name = name;
+           // user1.Password = password;
+            unitOfWork.userRepository.Add(user1);
+            // FacilityContext.Users.AddOrUpdate(user1);
+            
+        }
+
+        //[Route("CreateUser")]
+        //public IHttpActionResult CreateUser(int id, string name, string password, bool status)
+        //{
+        //    User user1 = new User();
+        //    user1.Name = name;
+        //    user1.Password = password;
+        //    user1.Status = status;
+        //    unitOfWork.userRepository.Add(user1);
+        //   // FacilityContext.Users.AddOrUpdate(user1);
+
+        //    if (user1 == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(user1);
+        //}
+
+        [Route("Update")]
+        public IHttpActionResult Update(int id, string name, string password, bool status)
+        {
+            User user2 = GetUsers().FirstOrDefault((p) => p.ID == id);
+            user2.Name = name;
+            user2.Password = password;
+            user2.Status = status;
+            unitOfWork.userRepository.Add(user2);
+
+            if (user2 == null)
+            {
+                return NotFound();
+            }
+            return Ok(user2);
+
+        }
         //// POST: api/Login
         //[ResponseType(typeof(User))]
         //public IHttpActionResult PostUser(User user)
