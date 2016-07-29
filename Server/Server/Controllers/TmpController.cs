2b56e3f4814fe1c731016b9067ed9f9e3d8336f0
Fertea Tudor Andrei <tudorandrei.fertea@tecknoworks.com>
2016-07-29 14:58:05 +0300

    Login method to temporary controller.

--- Server/Server/Controllers/TmpController.cs
+++ Server/Server/Controllers/TmpController.cs
@@ -12,23 +12,38 @@ namespace Server.Controllers
     {
         List<User> users = new List<User>
             {
-            new User{ID=1, Name="Carson", Password="clock",UserName="wee"},
-            new User{ID=2, Name="Carson1", Password="clock1",UserName="wee"},
-            new User{ID=3, Name="Carson2", Password="clock2",UserName="wee"},
+            new User{ID=1, Name="Carson", Password="clock",UserName="userName1"},
+            new User{ID=2, Name="Carson1", Password="clock1",UserName="userName2"},
+            new User{ID=3, Name="Carson2", Password="clock2",UserName="userName3"},
+            new User{ID=3, Name="Carson3", Password="clock3",UserName="userName4"},
+            new User{ID=3, Name="Carson4", Password="clock4",UserName="userName5"},
+            new User{ID=3, Name="Carson5", Password="clock5",UserName="userName6"},
+            new User{ID=3, Name="Carson6", Password="clock6",UserName="userName7"},
             };
         List<Field> fields = new List<Field>
             {
             new Field{ID=1, Name="ClujArena", Location="Cluj",Type="fotbal",
                 Availability =new List<DateTime> { new DateTime(2016,04,12),
                                                    new DateTime(2016,04,12) } },
-            new Field{ID=1, Name="Terapia", Location="Cluj",Type="fotbal"},
-            new Field{ID=1, Name="ClubTransilvania", Location="Cluj",Type="tenis"},
-            new Field{ID=1, Name="WinnerSportsClub", Location="Cluj",Type="tenis"},
+            new Field{ID=2, Name="Terapia", Location="Cluj",Type="fotbal"},
+            new Field{ID=3, Name="ClubTransilvania", Location="Cluj",Type="tenis"},
+            new Field{ID=4, Name="WinnerSportsClub", Location="Cluj",Type="tenis"},
             };
 
         public List<Field> GetFields()
         {
             return fields;
+        }
+        [HttpGet]
+        public IHttpActionResult Login(string userName, string password)
+        {
+
+            var user = users.FirstOrDefault((q) => q.UserName.Equals(userName) && q.Password.Equals(password));
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
         }
         public IHttpActionResult GetFieldByName(string name)
         {
