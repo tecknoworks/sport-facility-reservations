fc030417fb75b7af86ccd9a83ddd9fff9b22214c
Veronica Gliga <veronica.gliga@tecknoworks.com>
2016-08-12 14:29:37 +0300

    Sign up with facebook.

--- Client/Client/Client.Droid/Resources/LoginPageRenderer.cs
+++ Client/Client/Client.Droid/Resources/LoginPageRenderer.cs
@@ -1,4 +1,7 @@
 ﻿using Android.App;
+using Client.Models;
+using Client.Services;
+using Client.ViewModels;
 using Client.Views;
 using Newtonsoft.Json.Linq;
 using System;
@@ -9,15 +12,21 @@ using System.Threading.Tasks;
 using Xamarin.Auth;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
+using Microsoft.Practices.Unity;
 
 [assembly: ExportRenderer(typeof(Login), typeof(Client.Droid.LoginPageRenderer))]
 namespace Client.Droid
 {
     public class LoginPageRenderer: PageRenderer
     {
-
+        public static string id;
+        LoginViewModel vm ;
+        
         public LoginPageRenderer()
         {
+
+            vm = App.Container.Resolve<LoginViewModel>();
+
             var activity = this.Context as Activity;
             var auth = new OAuth2Authenticator(
                 clientId: "1251742814858891",
@@ -33,12 +42,15 @@ namespace Client.Droid
                     var expiresIn = Convert.ToDouble(eventArgs.Account.Properties["expires_in"]);
                     var expiryDate = DateTime.Now + TimeSpan.FromSeconds(expiresIn);
 
-                    var request = new OAuth2Request("GET", new Uri("https://graph.facebook.com/me"), null, eventArgs.Account);
+                    var request = new OAuth2Request("GET", new Uri("https://graph.facebook.com/me?fields=id,first_name,email"), null, eventArgs.Account);
                     var response = await request.GetResponseAsync();
                     var obj = JObject.Parse(response.GetResponseText());
-                    var id = obj["id"].ToString().Replace("\"", ""); // Id has extraneous quotation marks
-                    var name = obj["name"].ToString().Replace("\"", "");
-                    var token = obj["token"].ToString().Replace("\"", "");
+                    id = obj["id"].ToString().Replace("\"", ""); // Id has extraneous quotation marks
+                    var name = obj["first_name"];
+                    var username = obj["email"].ToString();
+                    User user = new User(id, username);
+                    vm.GetUser(user);
+                    //App.Instance.SaveToken(eventArgs.Account.Properties["access_token"]);
                     await App.NavigateToProfile(string.Format("Hello {0}", name));
                 }
                 else
